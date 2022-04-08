a0d1f8aecad07836bf84bf0cf52453251a29abb2
Fausto Luna <faustolunalima@hotmail.com>
2022-04-08 16:23:33 -0300

    Adicionando script e efeitos para voltar ao top da pagina 'NewBorn'

---
+++ js/backTop/backTop.js
@@ -0,0 +1,13 @@
+const btnBackTop = document.querySelector('[data-anima="scroll"]');
+
+function ativabtnBackTop(){
+    const screenPosition = btnBackTop.getBoundingClientRect().top;
+    
+    if(screenPosition < -200) {
+        btnBackTop.classList.add('show_button');
+    }else{
+        btnBackTop.classList.remove('show_button');
+    };
+};
+
+window.addEventListener('scroll', ativabtnBackTop);
